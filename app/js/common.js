c29c4f6bdf7148fad16fef81198ad4434f1f29bb
sergdoren <sergdoren@mail.ru>
2017-05-26 16:54:29 +0300

    C2

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
-function menu(menu, cls) {
-    var menu = $('.header__nav'),
-        cls = 'header__nav_open';
+function menu(_menu, _cls) {
+    var menu = $(_menu),
+        cls = _cls;
 
     if(menu.hasClass(cls)){
         menu.removeClass(cls)
@@ -15,6 +15,10 @@ $().ready(function () {
     });
 
     $('.header__nav-burger').on('click', function () {
-        menu($('.header__nav'), 'header__nav_open');
+        menu('.header__nav', 'header__nav_open');
+    });
+    
+    $('.menu__bottom-href-burger').on('click', function () {
+        menu('.menu__bottom','menu__bottom_open');
     })
 });
